3be9cb0675128918e274bb270c919744f33db5fb
James Joseph Meldrum <meldrum.james@hotmail.com>
2021-02-11 13:23:36 +0000

    basic layout of opening page done

---
+++ screens/StartGameScreen.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { View, StyleSheet, Text, TextInput, Button } from "react-native";
+
+import Card from "../components/Card";
+
+const StartGameScreen = (props) => {
+  return (
+    <View style={styles.screen}>
+      <Text style={styles.title}>Start a new game</Text>
+      <Card style={styles.inputContainer}>
+        <Text>Select a number</Text>
+        <TextInput />
+        <View style={styles.buttonContainer}>
+          <Button title="reset" onPress={() => {}} />
+          <Button title="confirm" onPress={() => {}} />
+        </View>
+      </Card>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 10,
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 20,
+    marginVertical: 10,
+  },
+  inputContainer: {
+    width: 300,
+    maxWidth: "80%",
+    alignItems: "center",
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    width: "100%",
+    justifyContent: "space-between",
+    paddingHorizontal: 15,
+  },
+});
+
+export default StartGameScreen;
