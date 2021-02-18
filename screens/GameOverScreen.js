2f5b93ded4f5fa6e67eeb002640d2c787164d629
James Joseph Meldrum <meldrum.james@hotmail.com>
2021-02-18 17:45:32 +0000

    app completed

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,15 +1,37 @@
 import React from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import { View, Text, StyleSheet, Image } from "react-native";
+import { Colors } from "react-native/Libraries/NewAppScreen";
 import BodyText from "../components/BodyText";
+import MainButton from "../components/MainButton";
 import TitleText from "../components/TitleText";
 
 const GameOverScreen = (props) => {
   return (
     <View style={styles.screen}>
       <TitleText>The Game is Over!</TitleText>
-      <BodyText>Number of rounds: {props.roundsNumber}</BodyText>
-      <BodyText>Number was: {props.userNumber}</BodyText>
-      <Button title="NEW GAME" onPress={props.onRestart} />
+      <View style={styles.imageContainer}>
+        <Image
+          source={require("../assets/images/success.png")}
+          /*
+          source={{
+            uri:
+              "https://cdn.pixabay.com/photo/2016/05/05/23/52/mountain-summit-1375015_960_720.jpg",
+          }}
+          */
+          style={styles.image}
+          resizeMode="cover"
+        />
+      </View>
+      <View style={styles.resultContainer}>
+        <BodyText style={styles.resultText}>
+          Your phone took{" "}
+          <Text style={styles.highlight}>{props.roundsNumber}</Text> rounds to
+          guess the number{" "}
+          <Text style={styles.highlight}>{props.userNumber}</Text>
+        </BodyText>
+      </View>
+
+      <MainButton onPress={props.onRestart}>NEW GAME</MainButton>
     </View>
   );
 };
@@ -19,6 +41,31 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+  imageContainer: {
+    borderRadius: 150,
+    borderWidth: 3,
+    borderColor: "black",
+    width: 300,
+    height: 300,
+    overflow: "hidden",
+    marginVertical: 30,
+  },
+  resultContainer: {
+    marginHorizontal: 30,
+    marginBottom: 10,
+  },
+  resultText: {
+    textAlign: "center",
+    fontSize: 20,
+  },
+  highlight: {
+    color: Colors.primary,
+    fontFamily: "open-sans-bold",
   },
 });
 
